8759ecc1d783db3e857c292b6f6c5188a47d4eef
Walter-Haydock <41760518+Walter-Haydock@users.noreply.github.com>
2022-05-15 13:42:56 -0400

    Increased version to 1.0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = []
 
 setup(
     name="dsram",
-    version="1.0.3",
+    version="1.0.4",
     author="Walter Haydock",
     author_email="walter@deploy-securely.com",
     description="A quantitative cyber risk management calculator",
